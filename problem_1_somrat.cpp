305058324fcc28af6aba4bc9e3ac1bfb00ad8bb6
Md. Shah Alam <95689721+shahalam141072@users.noreply.github.com>
2021-12-07 07:57:46 -0800

    Add files via upload

---
+++ problem_1_somrat.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+    int a , b , sum;
+
+    printf("Enter two number: ");
+    scanf("%d %d", &a,&b);
+
+    sum = a+b;
+    printf("Sum is: %d" , sum);
+
+
+    return 0;
+}
